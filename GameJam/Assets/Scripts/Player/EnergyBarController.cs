2085c0a46f7cba8c596197d63d235b8d2ce8cf8f
mategor195 <mategor195@student.polsl.pl>
2022-12-03 13:53:22 +0100

    Energy Bar

---
+++ GameJam/Assets/Scripts/Player/EnergyBarController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider slider;
+    public float hp;
+    public GameObject hpController;
+    void Start()
+    {
+        hpController = GameObject.FindGameObjectWithTag("HP");
+        hp = hpController.GetComponent<playersHp>().hp;
+        slider.maxValue = hp;
+        slider.value = hp;
+
+        var players= GameObject.FindGameObjectsWithTag("Player");
+        for (int i =0; i< players.Length; i++)
+        {
+            players[i].GetComponent<PlayerEventHandler>().OnPowerDownEvent.AddListener(BarDown);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void BarDown()
+    {
+        slider.value = hpController.GetComponent<playersHp>().hp;
+    }
+}
