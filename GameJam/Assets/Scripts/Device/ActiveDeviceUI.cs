d87df7be000c235b958d1c5cf72f5599384ba3db
Mateusz Cudzik <mattcudzikpl@gmail.com>
2022-12-03 11:28:14 +0100

    GOTOWE UI

--- GameJam/Assets/Scripts/Device/ActiveDeviceUI.cs
+++ GameJam/Assets/Scripts/Device/ActiveDeviceUI.cs
@@ -7,6 +7,16 @@ public class ActiveDeviceUI : MonoBehaviour
 
     private IActiveDevice activeDevice;
     private bool isUIActive;
+
+    [SerializeField]
+    private Sprite unChargedPointSprite;
+    [SerializeField]
+    private Sprite chargedPointSprite;
+    [SerializeField]
+    private GameObject chargePointObject;
+    [SerializeField]
+    private GameObject chargeBar;
+
     void Start()
     {
         activeDevice = transform.GetComponent<IActiveDevice>();
@@ -21,8 +31,29 @@ public class ActiveDeviceUI : MonoBehaviour
 
     private void UpdateUI()
     {
-        if (isUIActive)
+        if (!isUIActive)
         {
+            isUIActive = true;
+            int maxCharge = activeDevice.getMaxPowerLelvel();
+            for(int i = 0; i < maxCharge; i++)
+            {
+                if(maxCharge%2 != 0)
+                {
+                    GameObject point = Instantiate(chargePointObject, chargeBar.transform);
+                    //0.375
+                    point.transform.Translate(new Vector3(i * 0.375f - 0.375f*(maxCharge/2), 0, 0));
+                    
+                }
+                else
+                {
+                    GameObject point = Instantiate(chargePointObject, chargeBar.transform);
+                    
+                    point.transform.Translate(new Vector3(i * 0.375f - 0.1875f - 0.375f * ((maxCharge / 2)-1), 0, 0));
+                }
+                
+            }
+            
+
 
         }
     }
