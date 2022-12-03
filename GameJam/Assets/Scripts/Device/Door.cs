a4b8b167c953a41598484420e0f28bc0d786421f
Quecheri <quecheri@interia.pl>
2022-12-03 12:40:53 +0100

    Frst dor

--- GameJam/Assets/Scripts/Device/Door.cs
+++ GameJam/Assets/Scripts/Device/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UIElements;
 
 public class Door : IPassiveDevice
@@ -11,10 +12,12 @@ public class Door : IPassiveDevice
     {
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
+        GetComponent<ShadowCaster2D>().enabled = false;
     }
     protected override void isDepowered()
     {
         GetComponent<SpriteRenderer>().enabled = true;
         GetComponent<Collider2D>().enabled = true;
+        GetComponent<ShadowCaster2D>().enabled = true;
     }
 }
