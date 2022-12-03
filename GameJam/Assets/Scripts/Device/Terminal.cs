00eb7dda490bdd398df9d1a499c3e5817286f887
mategor195 <mategor195@student.polsl.pl>
2022-12-03 16:47:32 +0100

    Terminal seriallaized field

--- GameJam/Assets/Scripts/Device/Terminal.cs
+++ GameJam/Assets/Scripts/Device/Terminal.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Terminal : IPassiveDevice
@@ -7,12 +9,14 @@ public class Terminal : IPassiveDevice
     [SerializeField] protected Sprite poweredSprite;
     [SerializeField] protected Sprite depoweredSprite;
     [SerializeField] protected Canvas popUp;
+    [SerializeField] protected TextMeshProUGUI popTMP;
+    [SerializeField] protected String popUPText;
 
     protected override void isPowered()
     {
         GetComponent<SpriteRenderer>().sprite = poweredSprite;
         popUp.enabled = true;
-      
+        popTMP.text = popUPText;
 
     }
     protected override void isDepowered()
