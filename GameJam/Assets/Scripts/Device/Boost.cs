0f6ca6328ef93363bede273bfb809cc1a5b4e498
mategor195 <mategor195@student.polsl.pl>
2022-12-03 18:05:23 +0100

    cases done better

--- GameJam/Assets/Scripts/Device/Boost.cs
+++ GameJam/Assets/Scripts/Device/Boost.cs
@@ -12,41 +12,52 @@ public class Boost : Terminal
     // Start is called before the first frame update
     protected override void isPowered()
     {
+        popUp.enabled = true;
+       
         remainingTime = 5f;
         GetComponent<SpriteRenderer>().sprite = poweredSprite;
         if (previousEvent == 0)
         {
             GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>().intensity = 0.3f;
+            popTMP.text = "Bonus Light";
+            Debug.Log(popUp);
         }
         else if (previousEvent == 1)
         {
             GameObject.FindGameObjectWithTag("HP").GetComponent<playersHp>().hp += 3;
+            popTMP.text = "Bonus Energy";
+            Debug.Log(popUp);
         }
         else if (previousEvent == 2)
         {
             GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<KeyboardControlls>().movementSpeed = 15f;
             GameObject.FindGameObjectsWithTag("Player")[1].GetComponent<KeyboardControlls>().movementSpeed = 15f;
+            popTMP.text = "Additional Speed";
+            Debug.Log(popUp);
 
         }
         else if (previousEvent == 3)
         {
             GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<KeyboardControlls>().movementSpeed = 5f;
             GameObject.FindGameObjectsWithTag("Player")[1].GetComponent<KeyboardControlls>().movementSpeed = 5f;
-
+            popTMP.text = "Lowered Speed";
+            Debug.Log(popUp);
         }
 
     }
 
+
         void Update()
         {
-            if (remainingTime < 5f)
+            if (remainingTime > 0f)
             {
                 remainingTime -= Time.deltaTime;
             }
             else
             {
-                //GetComponent<SpriteRenderer>().sprite = depoweredSprite;
-                if (previousEvent == 0)
+            popUp.enabled = false;
+            //GetComponent<SpriteRenderer>().sprite = depoweredSprite;
+            if (previousEvent == 0)
                 {
                     GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>().intensity = 0.1f;
                 }
