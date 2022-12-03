1b4986ab6f240f7c1f80f3564b154b1d50662d70
jakub22222 <qba6666@gmail.com>
2022-12-03 08:16:24 +0100

    szansa szansa szansa szansa na sukces

--- GameJam/Assets/Scripts/Device/Boost.cs
+++ GameJam/Assets/Scripts/Device/Boost.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UIElements;
 
-public class Door : IPassiveDevice
+public class Boost : IPassiveDevice
 {
-    
     // Start is called before the first frame update
     protected override void isPowered()
     {
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
+        var light = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
+        light.intensity = 0.3f;
     }
     protected override void isDepowered()
     {
+        var light = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
+        light.intensity = 0.0f;
         GetComponent<SpriteRenderer>().enabled = true;
         GetComponent<Collider2D>().enabled = true;
     }
